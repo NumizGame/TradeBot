9e796fa9ef3083563e341832982b101bc7bcf93c
ThisIsMyShadow <69664612+NumizGame@users.noreply.github.com>
2023-04-10 20:29:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from yookassa import *
 
-token = '6221845943:AAEd_208UgNDxkd8rDCcSEGGZ6uRXO5oxGE'
+token = '...'
 
 # payments
-Configuration.account_id = '202542'
-Configuration.secret_key = 'test_qB2H1SFd94R0ljmQnyNX9OMpyWWLkdRqlvwj7L4C29I'
+Configuration.account_id = '...'
+Configuration.secret_key = '...'
 
 # payouts
-payout_account_id = '503583'
-payout_secret_key = 'test_*g7TAFWYMRQbGZAGIbF0WTDJ8s6dsfqAEv718Ju5Pde1M'
+payout_account_id = '...'
+payout_secret_key = '...'
